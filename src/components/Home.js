c01848286a5a3ac10ddb0249782db180c58d567f
Jarod Wilson <jaroddwilson@gmail.com>
2022-10-02 23:23:47 -0400

    Coding practice commented out

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,34 @@
-import React from "react";
 import { NavLink } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+
 
 function Home() {
 
   let photo = require(`../resources/crescentRunSign.jpeg`);
+
+  // const [time, setTime] = useState(0);
+  // const [paused, setPaused] = useState(false);
+
+  // useEffect(() => {
+  //   if (!paused) {
+  //     const intervalId = setInterval(() => {
+  //       setTime(time => time + 1);
+  //     }, 1000);
+  //     return function() {clearInterval(intervalId)}
+  //   }
+  // }, [paused])
+
+  // function increment() {
+  //   setTime(time => time + 1);
+  // }
+
+  // function decrement() {
+  //   setTime(time => time - 1);
+  // }
+
+  // function pause() {
+  //   setPaused(!paused)
+  // }
 
   return (
 
@@ -21,6 +46,13 @@ function Home() {
       >
         Search Now
       </NavLink>
+
+      {/* <p />
+      <h1>{time}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={pause}>Pause</button> */}
+
     </div>
 
   );
